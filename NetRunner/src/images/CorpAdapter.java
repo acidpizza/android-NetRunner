853343f8b7a1f8e54aaab9fd9d94655df1e92110
AcidPizza <AcidPizza@JSTYM-PC>
2013-06-11 23:45:44 +0800

    - Solved the rotated ICE cards performance issue (YAY! =D)

--- NetRunner/src/images/CorpAdapter.java
+++ NetRunner/src/images/CorpAdapter.java
@@ -4,12 +4,11 @@ import java.util.ArrayList;
 
 import tan.shawn.jerold.netrunner.R;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.FrameLayout;
 import android.widget.GridView;
 import android.widget.ImageView;
 
@@ -20,6 +19,9 @@ public class CorpAdapter extends BaseAdapter
         _context = c;
         _cardList = cardList;
         _iceTracker = iceTracker;
+        
+        _cardWidth = 108;
+        _cardHeight = 150;
         
         mat.postRotate(90);
     }
@@ -51,59 +53,81 @@ public class CorpAdapter extends BaseAdapter
     // create a new ImageView for each item referenced by the Adapter
     public View getView(int position, View convertView, ViewGroup parent) 
     {
+    	FrameLayout frame; // Use framelayout to hold imageview so that imageview can be rotated properly
         ImageView imageView;
+        
         if (convertView == null) 
         {  // if it's not recycled, initialize some attributes
-            imageView = new ImageView(_context);
-                        
+
+        	imageView = new ImageView(_context);
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
             imageView.setPadding(6, 6, 6, 6);
+            
+			imageView.setPivotX(0f);
+			imageView.setPivotY(0f);
+			imageView.setTag("IMAGE");
+
+			frame = new FrameLayout(_context);
+			frame.addView(imageView);
+			
+			//imageView.setBackgroundColor(Color.argb(255,255,0,0));
+			//frame.setBackgroundColor(Color.argb(255,0,255,0));
         } 
         else 
         {
-            imageView = (ImageView) convertView;
+			frame = (FrameLayout) convertView;
+			imageView = (ImageView) frame.findViewWithTag("IMAGE");
         }
         
-    	
+        imageView.setImageResource(_cardList.get(position));
+        
     	if(position < _iceTracker)
     	{
     		// Tapped
     		
     		/*
+    		// Difficult to fit image to layout due to ScaleType.MATRIX instead of CENTER_CROP
     		Matrix matrix=new Matrix();
             imageView.setScaleType(ScaleType.MATRIX);   //required
-            matrix.postScale(107.5f/225, 150f/314);
-            matrix.postRotate( 90f, 53.75f, 75f);
+            matrix.postScale( 108f / imageView.getDrawable().getIntrinsicWidth(), 150f / imageView.getDrawable().getIntrinsicHeight());
+            matrix.postRotate( 90f, 54, 75);
             imageView.setImageMatrix(matrix);
-            */
+ 			*/              		
     		
+    		/*
+    		// Works but performs poorly
     		Bitmap bMap = BitmapFactory.decodeResource(_context.getResources(), _cardList.get(position));
     		Bitmap bMapRotate = Bitmap.createBitmap(bMap, 0, 0,bMap.getWidth(),bMap.getHeight(), mat, true);
     		imageView.setImageBitmap(bMapRotate);
-
-            imageView.setLayoutParams(new GridView.LayoutParams(150, 108));
+    		imageView.setLayoutParams(new GridView.LayoutParams(_cardHeight, _cardWidth));
+    		 */
+    		
+			imageView.setRotation(-90f);
+			imageView.setTranslationY(_cardWidth);
+			imageView.setLayoutParams(new FrameLayout.LayoutParams(_cardWidth, _cardHeight));
+			
+			frame.setLayoutParams(new GridView.LayoutParams(_cardHeight, _cardWidth));
     	}
     	else
     	{
     		// Not Tapped 
-    		imageView.setImageResource(_cardList.get(position));
-    		//imageView.setRotation(0f);
-    		//imageView.setLayoutParams(new GridView.LayoutParams(150, 209));
-    		imageView.setLayoutParams(new GridView.LayoutParams(108, 150));
+			imageView.setRotation(0f);
+			imageView.setTranslationY(0);
+			imageView.setLayoutParams(new FrameLayout.LayoutParams(_cardWidth, _cardHeight));
+			
+			frame.setLayoutParams(new GridView.LayoutParams(_cardWidth, _cardHeight));
     	}
     	
     	if(_cardList.get(position) == R.drawable.nothing)
         {
         	imageView.setVisibility(View.INVISIBLE);
-        	//imageView.setClickable(false);
-        	//imageView.setEnabled(false);
         }
         else
         {
         	imageView.setVisibility(View.VISIBLE);
         }
     	        
-        return imageView;
+    	return frame;
     }
     
     public void SetIceTracker(int iceTracker)
@@ -116,4 +140,6 @@ public class CorpAdapter extends BaseAdapter
     private Context _context;
     private ArrayList<Integer> _cardList;
     private int _iceTracker = 0;
+    private int _cardWidth = 300;
+    private int _cardHeight = 418;
 }
